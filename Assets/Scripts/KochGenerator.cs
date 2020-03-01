f92a6c7d07951dcd2901f80bb7b9e926e253eecc
pierre.roy <pierre.roy@epitech.eu>
2020-03-01 17:14:46 +0100

    Koch fractals generator & renderer

---
+++ Assets/Scripts/KochGenerator.cs
@@ -0,0 +1,242 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace KochFractals
+{
+    public class KochGenerator : MonoBehaviour
+    {
+        #region Data Structures
+
+        protected enum Axis { X, Y, Z }
+
+        protected enum Initiator
+        {
+            Triangle,
+            Square,
+            Pentagon,
+            Hexagon,
+            Heptagon,
+            Octagon
+        }
+
+        private struct Rotation
+        {
+            public Vector3 direction;
+            public Vector3 axis;
+
+            public Rotation(Vector3 direction, Vector3 axis)
+            {
+                this.direction = direction;
+                this.axis = axis;
+            }
+        }
+
+        private struct InitiatorInfo
+        {
+            public int edgeCount;
+            public float initialRotation;
+
+            public InitiatorInfo(int edgeCount, float initialRotation)
+            {
+                this.edgeCount = edgeCount;
+                this.initialRotation = initialRotation;
+            }
+        }
+
+        public struct LineSegment
+        {
+            public Vector3 start;
+            public Vector3 end;
+            public Vector3 direction;
+            public float length;
+        }
+
+        private Dictionary<Axis, Rotation> _rotations = new Dictionary<Axis, Rotation>()
+        {
+            { Axis.X, new Rotation(new Vector3(1, 0, 0), new Vector3(0, 0, 1)) },
+            { Axis.Y, new Rotation(new Vector3(0, 1, 0), new Vector3(1, 0, 0)) },
+            { Axis.Z, new Rotation(new Vector3(0, 0, 1), new Vector3(0, 1, 0)) }
+        };
+
+
+        private static Dictionary<Initiator, InitiatorInfo> _initiators = new Dictionary<Initiator, InitiatorInfo>()
+        {
+            { Initiator.Triangle, new InitiatorInfo(3, 0f) },
+            { Initiator.Square, new InitiatorInfo(4, 45f) },
+            { Initiator.Pentagon, new InitiatorInfo(5, 36f) },
+            { Initiator.Hexagon, new InitiatorInfo(6, 30f) },
+            { Initiator.Heptagon, new InitiatorInfo(7, 25.71428f) },
+            { Initiator.Octagon, new InitiatorInfo(8, 22.5f) }
+        };
+
+        #endregion Data Structures
+
+        #region Serialized Fields
+
+        [SerializeField]
+        private Axis _axis = Axis.Z;
+
+        [SerializeField]
+        private Initiator _initiatorType = Initiator.Triangle;
+
+        [SerializeField]
+        private float _initiatorSize = 1f;
+
+        [SerializeField]
+        private AnimationCurve _generator = null;
+
+        #endregion Serialized Fields
+
+        #region Private Fields
+
+        private InitiatorInfo _initiator = default;
+        private Rotation _rotation = default;
+
+        protected Vector3[] _currentPositions = null;
+        protected Vector3[] _targetPositions = null;
+
+        private List<LineSegment> _lineSegments = null;
+        private Keyframe[] _keys = null;
+
+        protected int _generationSteps = 0;
+
+        #endregion Private Fields
+
+        #region MonoBehaviour
+
+        private void Awake()
+        {
+            InitializeGenerator();
+            InitializePositions();
+            InitializeSegments();
+        }
+
+        #endregion MonoBehaviour
+
+        #region Private Methods
+
+        private void InitializeGenerator()
+        {
+            _initiator = _initiators[_initiatorType];
+            _rotation = _rotations[_axis];
+        }
+
+        private void InitializeSegments()
+        {
+            _lineSegments = new List<LineSegment>();
+            _keys = _generator.keys;
+        }
+
+        private void InitializePositions()
+        {
+            /// Fetch initiator and axis info
+            _currentPositions = new Vector3[_initiator.edgeCount + 1];
+            _targetPositions = new Vector3[_initiator.edgeCount + 1];
+
+            /// Apply initial rotation to align with axis
+            _rotation.direction = Quaternion.AngleAxis(_initiator.initialRotation, _rotation.axis) * _rotation.direction;
+
+            /// Create shape edges
+            for (int i = 0; i < _initiator.edgeCount; i++)
+            {
+                _currentPositions[i] = _rotation.direction * _initiatorSize;
+                _rotation.direction = Quaternion.AngleAxis(360f / _initiator.edgeCount, _rotation.axis) * _rotation.direction;
+            }
+
+            /// Resolve last edge
+            _currentPositions[_initiator.edgeCount] = _currentPositions[0];
+            _targetPositions = _currentPositions;
+        }
+
+        protected void Generate(Vector3[] positions, bool outwards, float sizeMultiplier)
+        {
+            _lineSegments.Clear();
+
+            for (int i = 0; i < positions.Length - 1; i++)
+            {
+                LineSegment segment = new LineSegment
+                {
+                    start = positions[i],
+                    end = (i == positions.Length - 1) ? positions[0] : positions[i + 1]
+                };
+
+                segment.direction = (segment.end - segment.start).normalized;
+                segment.length = Vector3.Distance(segment.end, segment.start);
+                _lineSegments.Add(segment);
+            }
+
+            List<Vector3> newPos = new List<Vector3>();
+            List<Vector3> targetPos = new List<Vector3>();
+
+            for (int i = 0, length = _lineSegments.Count; i < length; i++)
+            {
+                LineSegment currentSegment = _lineSegments[i];
+
+                newPos.Add(currentSegment.start);
+                targetPos.Add(currentSegment.start);
+
+                /// Resolve new segment positions with animation curve.
+                for (int j = 0, keyCount = _keys.Length - 1; j < keyCount; j++)
+                {
+                    Keyframe currentKey = _keys[j];
+
+                    float moveAmount = currentSegment.length * currentKey.time;
+                    float heightAmount = (currentSegment.length * currentKey.value) * sizeMultiplier;
+
+                    Vector3 movePos = currentSegment.start + (currentSegment.direction * moveAmount);
+                    Vector3 moveDir = Quaternion.AngleAxis(outwards ? -90 : 90, _rotation.axis) * currentSegment.direction;
+
+                    newPos.Add(movePos);
+                    targetPos.Add(movePos + (moveDir * heightAmount));
+                }
+            }
+
+            /// Resolve last segment
+            newPos.Add(_lineSegments[0].start);
+            targetPos.Add(_lineSegments[0].start);
+
+            /// Apply changes to edge position arrays
+            _currentPositions = new Vector3[newPos.Count];
+            _targetPositions = new Vector3[targetPos.Count];
+            _currentPositions = newPos.ToArray();
+            _targetPositions = targetPos.ToArray();
+
+            /// Increment steps
+            _generationSteps++;
+        }
+
+        #endregion Private Methods
+
+        #region Editor
+
+        private void OnDrawGizmos()
+        {
+            InitiatorInfo initiator = _initiators[_initiatorType];
+            Vector3[] edges = new Vector3[initiator.edgeCount];
+            Rotation rotation = _rotations[_axis];
+
+            /// Apply initial rotation to align with axis
+            rotation.direction = Quaternion.AngleAxis(initiator.initialRotation, rotation.axis) * rotation.direction;
+
+            /// Create shape edges
+            for (int i = 0; i < initiator.edgeCount; i++)
+            {
+                edges[i] = rotation.direction * _initiatorSize;
+                rotation.direction = Quaternion.AngleAxis(360f / initiator.edgeCount, rotation.axis) * rotation.direction;
+            }
+
+            /// Initialize gizmos
+            Gizmos.color = Color.white;
+            Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+
+            /// Draw shape edges
+            for (int i = 0; i < initiator.edgeCount; i++)
+            {
+                Vector3 end = (i < initiator.edgeCount - 1) ? edges[i + 1] : edges[0];
+                Gizmos.DrawLine(edges[i], end);
+            }
+        }
+
+        #endregion Editor
+    }
+}
---
+++ Assets/Scripts/KochRenderer.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+namespace KochFractals
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class KochRenderer : KochGenerator
+    {
+        [Range(0, 1)]
+        [SerializeField]
+        private float _lerpAmount = 0f;
+
+        [SerializeField]
+        private float _generationSizeMultiplier = 1f;
+
+        [Space]
+        [SerializeField]
+        private KeyCode _generateInwards = KeyCode.I;
+
+        [SerializeField]
+        private KeyCode _generateOutwards = KeyCode.O;
+
+        private LineRenderer _lineRenderer = null;
+
+        private Vector3[] _lerpedPositions = null;
+
+        private void Start()
+        {
+            _lineRenderer = GetComponent<LineRenderer>();
+            _lineRenderer.loop = true;
+            _lineRenderer.enabled = true;
+            _lineRenderer.useWorldSpace = false;
+            _lineRenderer.positionCount = _currentPositions.Length;
+            _lineRenderer.SetPositions(_currentPositions);
+        }
+
+        private void Update()
+        {
+            /// Lerp current positions towards target positions.
+            if (_generationSteps != 0)
+            {
+                for (int i = 0, length = _currentPositions.Length; i < length; i++)
+                {
+                    _lerpedPositions[i] = Vector3.Lerp(_currentPositions[i], _targetPositions[i], _lerpAmount);
+                }
+
+                _lineRenderer.SetPositions(_lerpedPositions);
+            }
+
+            ProcessInput();
+        }
+
+        private void ProcessInput()
+        {
+            if (Input.GetKeyDown(_generateInwards))
+            {
+                Generate(_targetPositions, false, _generationSizeMultiplier);
+                UpdateRenderer();
+            }
+
+            if (Input.GetKeyDown(_generateOutwards))
+            {
+                Generate(_targetPositions, true, _generationSizeMultiplier);
+                UpdateRenderer();
+            }
+        }
+
+        private void UpdateRenderer()
+        {
+            _lerpedPositions = new Vector3[_currentPositions.Length];
+            _lineRenderer.positionCount = _currentPositions.Length;
+            _lineRenderer.SetPositions(_currentPositions);
+            _lerpAmount = 0;
+        }
+    }
+}
